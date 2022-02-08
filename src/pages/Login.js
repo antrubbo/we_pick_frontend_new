9c5333f50c659c971caed2db01415421da08acb4
Anthony Rubbo <anthony.rubbo@gmail.com>
2022-02-08 16:42:15 -0500

    major work on login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,34 +4,30 @@ import { Link } from "react-router-dom"
 const API = process.env.REACT_APP_API_URL
 
 const Login = () => {
+    let loggedInUser = useRef()
     const [user, setUser] = useState({
         username: "",
         password: ""
     })
-    const [requestedUser, setRequestedUser] = useState({})
-    let loggedInUser = useRef()
-    loggedInUser.current = requestedUser
     console.log(loggedInUser.current)
 
-    const logInUserDetails = (evt) => {
+    const handleLoginForm = (evt) => {
         setUser({ ...user, [evt.target.id]: evt.target.value })
     }
 
     const handleSubmit = (evt) => {
         evt.preventDefault()
-        fetch(`${API}/api/users?username=${user.username}`)
-            // method: "POST",
-            // headers: {
-            //     "Content-Type": "application/json"
-            // },
-            // body: JSON.stringify(user)
+        fetch(`${API}/api/users/login?username=${user.username}`)
         .then(r => r.json())
         .then(user => {
-            setRequestedUser(user)
+            if(user.error){
+                console.log(user.error)
+                setUser({username: "", password: ""})
+            } else {
+                loggedInUser.current = user
+                setUser({username: "", password: ""})
+            }
         })
-        .catch(err => console.log(err))
-        // Reset Form Fields:
-        // setUser({email: "", password: ""})
     }
 
     return(
@@ -39,10 +35,10 @@ const Login = () => {
             <h1>This is the LOGIN page</h1>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username</label>
-                <input onChange={logInUserDetails} type="text" id="username" value={user.username}/>
+                <input onChange={handleLoginForm} type="text" id="username" value={user.username}/>
                 <br />
                 <label htmlFor="password">Password</label>
-                <input onChange={logInUserDetails} type="password" id="password" value={user.password}/>
+                <input onChange={handleLoginForm} type="password" id="password" value={user.password}/>
                 <br />
                 <button>Submit</button>
             </form>
